c2d84aa5a0bed2f38f893ada865e4943382ef3a2
Simon <44614995+simon2000234@users.noreply.github.com>
2021-09-15 10:10:33 +0200

    Initial commit

---
+++ HotelBooking.UnitTests/BookingManagerTests.cs
@@ -0,0 +1,39 @@
+using System;
+using HotelBooking.Core;
+using HotelBooking.UnitTests.Fakes;
+using Xunit;
+
+namespace HotelBooking.UnitTests
+{
+    public class BookingManagerTests
+    {
+        private IBookingManager bookingManager;
+
+        public BookingManagerTests(){
+            DateTime start = DateTime.Today.AddDays(10);
+            DateTime end = DateTime.Today.AddDays(20);
+            IRepository<Booking> bookingRepository = new FakeBookingRepository(start, end);
+            IRepository<Room> roomRepository = new FakeRoomRepository();
+            bookingManager = new BookingManager(bookingRepository, roomRepository);
+        }
+
+        [Fact]
+        public void FindAvailableRoom_StartDateNotInTheFuture_ThrowsArgumentException()
+        {
+            DateTime date = DateTime.Today;
+            Assert.Throws<ArgumentException>(() => bookingManager.FindAvailableRoom(date, date));
+        }
+
+        [Fact]
+        public void FindAvailableRoom_RoomAvailable_RoomIdNotMinusOne()
+        {
+            // Arrange
+            DateTime date = DateTime.Today.AddDays(1);
+            // Act
+            int roomId = bookingManager.FindAvailableRoom(date, date);
+            // Assert
+            Assert.NotEqual(-1, roomId);
+        }
+
+    }
+}
---
+++ HotelBooking.WebApi/Controllers/RoomsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using HotelBooking.Core;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HotelBooking.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RoomsController : Controller
+    {
+        private readonly IRepository<Room> repository;
+
+        public RoomsController(IRepository<Room> repos)
+        {
+            repository = repos;
+        }
+
+        // GET: api/rooms
+        [HttpGet]
+        public IEnumerable<Room> Get()
+        {
+            return repository.GetAll();
+        }
+
+        // GET api/rooms/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var item = repository.Get(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        // DELETE api/rooms/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            if (id > 0)
+            {
+                repository.Remove(id);
+                return NoContent();
+            }
+            else {
+                return BadRequest();
+            }
+        }
+
+    }
+}
